b75f08398a8d18b382036d21979ccf654bbd1503
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-05-01 15:32:55 +0200

    add power dog addon

---
+++ power-dog-mqtt/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bashio
+export POWER_DOG_HOST=$(bashio::config "power_dog_host")
+export POWER_DOG_PASSWORD=$(bashio::config "power_dog_password")
+
+export MQTT_HOST=$(bashio::services mqtt "host")
+export MQTT_AUTH_PASSWORD=$(bashio::services mqtt "password")
+export MQTT_AUTH_USERNAME=$(bashio::services mqtt "username")
+export MQTT_PORT=$(bashio::services mqtt "port")
+
+set -e
+
+# Trap any exit signals (including errors in background processes)
+trap 'exit 1' ERR
+
+# Start the first PHP command in the background
+php83 /opt/power-dog run &
+
+# Wait for all background jobs to finish
+wait
